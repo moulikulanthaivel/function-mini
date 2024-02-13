efda1ac35da5d152bcc2682eda7631ef0bfe593e
moulikulanthaivel <moulikulanthaivel@gmail.com>
2024-02-13 09:08:42 -0800

    function-mini

---
+++ function.js
@@ -0,0 +1,34 @@
+function colorChange1()
+{
+
+let btn_ref = document.getElementById('clr1');
+btn_ref.style.backgroundColor = 'green';
+btn_ref.style.color= 'white';
+console.log(btn_ref);
+
+}
+
+function colorChange2()
+{
+
+let btn_ref2 = document.getElementById('clr2');
+btn_ref2.style.backgroundColor = 'blue';
+btn_ref2.style.color= 'white';
+console.log(btn_ref2);
+
+}
+
+function colorChange3()
+{
+
+let btn_ref3 = document.getElementById('clr3');
+btn_ref3.style.backgroundColor = 'red';
+btn_ref3.style.color= 'white';
+console.log(btn_ref3);
+
+}
+
+let constNumber = '';
+
+
+
